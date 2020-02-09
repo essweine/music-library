5b53105ceaf59565fe12593c150a4fe3123d6eb0
Elizabeth <essweine@georgetown.edu>
2020-02-09 09:59:17 -0500

    remove file for testing

--- application/player/tracks.py
+++
@@ -1,8 +0,0 @@
-import time
-import os
-from collections import namedtuple
-dirname = "/home/essweine/2006-10-10 Washington"
-Track = namedtuple('Track', [ "filename" ])
-tracks = sorted([ Track(os.path.join(dirname, d)) for d in os.listdir(dirname) ])
-
-
